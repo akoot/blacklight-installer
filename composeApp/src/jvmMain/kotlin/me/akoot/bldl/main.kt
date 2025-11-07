d3f0872a492450ea53a5c17cc6021080a2fd3c22
Akoot <akoot@akoot.me>
2025-11-07 13:57:17 -0500

    Init Commit

---
+++ composeApp/src/jvmMain/kotlin/me/akoot/bldl/main.kt
@@ -0,0 +1,13 @@
+package me.akoot.bldl
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "BlacklightInstaller",
+    ) {
+        App()
+    }
+}
